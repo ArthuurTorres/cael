5399fe23d5ce4b177f5982f594b60bf03a2b1ddf
Arthur Torres <arthur-2008@hotmail.com>
2023-06-28 04:53:20 -0300

    add idk

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,42 +1,47 @@
 import './Form.css'
 import { useState } from 'react';
+import InputMask from 'react-input-mask';
+import { useForm } from 'react-hook-form'
 
 const Form = () => {
 
-  const sendform = async () => {await fetch('https://caelconsultoria.online/api/sendEmail?'+ new URLSearchParams({nome: document.getElementById('nome').value,
-  email: document.getElementById('email').value,
-  telefone: document.getElementById('tel').value,
-  plano: document.getElementById('appearance-select').value,
-  }),{method:'POST'})
-  .then(json => console.log(json)).catch(error => console.log(error))}
+  const sendform = async () => {
 
+    await fetch(
+      "https://caelconsultoria.online/api/sendEmail?" +
+        new URLSearchParams(new FormData(document.getElementById("form"))),
+      { method: "POST" }
+    )
+      .then((json) => console.log(json))
+      .catch((error) => console.log(error));
+    }
 
+  
   const [enviando, setEnviando] = useState(false);
+  const {register, handleSubmit, formState:{errors}} = useForm();
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    setEnviando(true);
-    setTimeout(() => {
-      setEnviando(false);
-    }, 9999999);
-  };
+
 
   return (
     <section className="containerform">
       <div className="bg-form">
       <div className="container">
-        <form className="form" id="form" onSubmit={handleSubmit} onClick={sendform}>
+        <form className="form" id="form" onSubmit={handleSubmit(sendform)}>
           <h2 className='t1'>Preencha o Formulário e 
           <br></br>
           <span className="text-detail">receba sua cotação.</span></h2>
-          <input name="Nome" type="text" id="nome" placeholder='Nome' />
-          <input name="Email" id="email" placeholder='Email' /> 
-          <input name="Telefone" id="tel" placeholder='Telefone' />
+          <input name="Nome" type="text" id="nome" placeholder='Nome' {...register('nome',{required:true, minLength:3})} />
+          {errors.nome && errors.nome.type==='required' ? <span className="error t1">*Este campo é Requerido</span> : ''}
+          {errors.nome && errors.nome.type==='minLength' ? <span className="error t1">*Este nome é muito Curto</span> : ''}
+          <input name="Email" id="email" placeholder='Email' {...register('email',{required:true, pattern: {value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+.[A-Z]{2,}$/i, }})} /> 
+          {errors.email && errors.email.type==='required' ? <span className="error t1">*Preencha este campo</span> : ''}
+          {errors.email && errors.email.type==='pattern' ? <span className="error t1">*Preencha com um email válido</span> : ''}
+          <InputMask mask="(99) 9999-9999" name="Telefone" id="tel" placeholder='Telefone' />
           <select name="Tipo de Plano" id="appearance-select">
           <option>Selecione o plano</option>
           <option>Individual</option>
           <option>Familiar</option>
-          <option>Empreserial</option>
+          <option>Empresarial</option>
           <option>Odontológico</option>
           </select>
           <button 
